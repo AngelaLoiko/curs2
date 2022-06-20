2380f8a2cb5eaab8cfd52fa7f332e73b4fc45a4c
Angela Loiko <angelaloiko@gmail.com>
2022-06-20 18:52:55 +0200

    script

--- db/create_db.sql
+++ db/create_db.sql
@@ -4,16 +4,23 @@ CREATE TABLE IF NOT EXISTS status (
 	id_status smallint PRIMARY KEY,
 	status varchar(20) NOT NULL
 	);
+COMMENT ON TABLE status IS 'Справочник состояний кандидатов';
 
 CREATE TABLE IF NOT EXISTS relation (
 	id_relation smallint PRIMARY KEY,
 	relation varchar(40) NOT NULL
 	);
+COMMENT ON TABLE relation IS 'Справочник статусов пользователей Вконтакте';
 
-CREATE TABLE IF NOT EXIST sex (
+CREATE TABLE IF NOT EXISTS sex (
 	id_sex smallint PRIMARY KEY,
 	sex varchar(10)
 	);
+COMMENT ON TABLE sex IS 'Справочник пола';
+INSERT INTO sex (id_sex, sex) VALUES(0, 'любой');
+INSERT INTO sex (id_sex, sex) VALUES(1, 'женский');
+INSERT INTO sex (id_sex, sex) VALUES(2, 'мужской');
+
 
 CREATE TABLE IF NOT EXISTS users (
 	id_user serial PRIMARY KEY,
@@ -21,12 +28,14 @@ CREATE TABLE IF NOT EXISTS users (
 	id_vk_str varchar(80) NOT NULL,
 	first_name varchar(80),
 	last_name varchar(80),
-	id_sex smallint REFERENCES sex (id_sex),
+	id_sex smallint NOT NULL REFERENCES sex (id_sex) default 0,
 	id_city int,
 	bdate date,
 	id_relation int REFERENCES relation (id_relation),
 	url varchar(160) NOT NULL
 	);
+COMMENT ON TABLE users IS 'Таблица пользователей Вконтакте';
+
 
 CREATE TABLE IF NOT EXISTS user_candidate (
 	id_user_candidate serial PRIMARY KEY,
@@ -35,6 +44,7 @@ CREATE TABLE IF NOT EXISTS user_candidate (
 	id_status smallint REFERENCES status (id_status),
 	search_date timestamp
 	);
+COMMENT ON TABLE user_candidate IS 'Таблица кандидатов';
 
 CREATE TABLE IF NOT EXISTS photo (
 	id_photo serial PRIMARY KEY,
@@ -43,6 +53,7 @@ CREATE TABLE IF NOT EXISTS photo (
 	likes_count int,
 	liked bool
 	);
+COMMENT ON TABLE photo IS 'Таблица фотографий пользователей';
 
 CREATE TABLE IF NOT EXISTS req_params (
 	id_user int PRIMARY KEY REFERENCES users (id_user),
@@ -52,6 +63,7 @@ CREATE TABLE IF NOT EXISTS req_params (
 	age_to int,
 	id_relation smallint REFERENCES relation (id_relation)
 	);
+COMMENT ON TABLE req_params IS 'Таблица параметров запроса';
 
 INSERT INTO status (id_status, status) 
 VALUES
