238f3b1e4d335dbac1467353d7ab30a572e604f2
Angela Loiko <angelaloiko@gmail.com>
2022-06-23 15:07:27 +0200

    remove keys

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 user_record = {
     "id" : "472442796",
-    "TOKEN_VK" : "a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd",
+    "TOKEN_VK" : "",
     "URL" : 'https://api.vk.com/method/',
     "version_api_vk" : "5.131"
 }
 
 group_record = {
-    "TOKEN_GROUP" : "vk1.a.XYJPguSQ0c-PgscuH3w8XonRfdoMZ4k6N0W1NCVwO2al4m0Coa4a4tJZRVhtCfIM7NaeZYANPvBlhihN5DU20HBB2vckoL-6oqx2r-SnzZ-lqog68CtgUQlNuHzldXNmFUVt28GyTmvBxhHP8cade7hONnNMCIixyqA5LDdyfKGnVcRscc811xxyjvW8QN1a",
+    "TOKEN_GROUP" : "",
     "URL" : 'https://api.vk.com/method/',
     "version_api_vk" : "5.131"
 }
