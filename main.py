abfb3c4594cbf06d66ee98056b1db58d2716e1d9
Angela Loiko <angelaloiko@gmail.com>
2022-06-23 03:51:29 +0200

    Interaction with VK

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-if __name__ == '__main__':
-    pass
+from random import randrange
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+import settings
+
+
+#token = input('Token: ')
+
+vk = vk_api.VkApi(token=settings.group_record["TOKEN_VK"])
+longpoll = VkLongPoll(vk)
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        if event.to_me:
+            request = event.text
+
+            if request == "привет":
+                write_msg(event.user_id, f"Хай, {event.user_id}")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
